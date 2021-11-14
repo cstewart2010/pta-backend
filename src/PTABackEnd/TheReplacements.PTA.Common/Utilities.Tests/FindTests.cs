6273d6dc90081a3aae51b288b5f3727f39d16712
Chahlie <cstewart2010@gmail.com>
2021-11-14 02:58:11 -0600

    Completed FindTests unit tests

--- src/PTABackEnd/TheReplacements.PTA.Common/Utilities.Tests/FindTests.cs
+++ src/PTABackEnd/TheReplacements.PTA.Common/Utilities.Tests/FindTests.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 using Xunit;
 using Xunit.Abstractions;
@@ -66,14 +67,21 @@ namespace TheReplacements.PTA.Common.Utilities.Tests
             }
 
             Logger.WriteLine($"Retrieving npc ids {string.Join(",", npcIds)}");
-            var retrievedNpcs = DatabaseUtility.FindNpcs(npcIds);
-            foreach (var npc in retrievedNpcs)
+            var retrievedNpcsIds = DatabaseUtility.FindNpcs(npcIds).Select(npc => npc.NPCId);
+            foreach (var npcId in retrievedNpcsIds)
             {
-                DatabaseUtility.DeleteNpc(npc.NPCId);
+                DatabaseUtility.DeleteNpc(npcId);
             }
+
             Logger.WriteLine($"Verifying retrieved list of npcs are from the original set");
-            Assert.True(retrievedNpcs.All(npc => npcIds.Contains(npc.NPCId)));
-            Assert.Equal(npcCount, retrievedNpcs.Count());
+            Assert.True(retrievedNpcsIds.All(npcId => npcIds.Contains(npcId)));
+            Assert.Equal(npcCount, retrievedNpcsIds.Count());
+        }
+
+        [Fact]
+        public void FindNpcs_NullIds_ThrowsArgumentNullException()
+        {
+            Assert.Throws<ArgumentNullException>(() => DatabaseUtility.FindNpcs(null));
         }
 
         [Fact]
@@ -95,6 +103,47 @@ namespace TheReplacements.PTA.Common.Utilities.Tests
             Assert.Null(DatabaseUtility.FindPokemonById(id));
         }
 
+        [Theory]
+        [InlineData(0)]
+        [InlineData(1)]
+        [InlineData(5)]
+        public void FindPokemonByTrainerId_ValidIds_PokemonCount(int pokemonCount)
+        {
+            var trainer = GetTestTrainer();
+            DatabaseUtility.TryAddTrainer(trainer, out _);
+
+            Logger.WriteLine($"Adding {pokemonCount} pokemon to trainer id {trainer.TrainerId}");
+            var pokemonIds = new List<string>();
+            for (int i = 0; i < pokemonCount; i++)
+            {
+                var pokemon = GetTestPokemon();
+                pokemon.TrainerId = trainer.TrainerId;
+                DatabaseUtility.TryAddPokemon(pokemon, out _);
+                pokemonIds.Add(pokemon.PokemonId);
+            }
+
+            Logger.WriteLine($"Retrieving list of pokemonIds with trainer id {trainer.TrainerId}");
+            var retrievedPokemonIds = DatabaseUtility.FindPokemonByTrainerId(trainer.TrainerId).Select(pokemon => pokemon.PokemonId);
+            DatabaseUtility.DeleteTrainer(trainer.TrainerId);
+            foreach (var pokemonId in retrievedPokemonIds)
+            {
+                DatabaseUtility.DeletePokemon(pokemonId);
+            }
+
+            Logger.WriteLine($"Verifying that the retrieved list matcheds the original set");
+            Assert.True(retrievedPokemonIds.All(pokemonId => retrievedPokemonIds.Contains(pokemonId)));
+            Assert.Equal(pokemonCount, retrievedPokemonIds.Count());
+        }
+
+        [Fact]
+        public void FindPokemonByTrainerId_NullId_EmptyList()
+        {
+            Logger.WriteLine($"Retrieving pokemon list");
+            var pokemonIds = DatabaseUtility.FindPokemonByTrainerId(null);
+            Logger.WriteLine($"Verifying the pokemon list is empty");
+            Assert.Empty(pokemonIds);
+        }
+
         [Fact]
         public void FindTrainerById_ValidId_NotNull()
         {
@@ -112,6 +161,93 @@ namespace TheReplacements.PTA.Common.Utilities.Tests
         {
             Logger.WriteLine($"Retrieving npc id {id}");
             Assert.Null(DatabaseUtility.FindTrainerById(id));
+        }
+
+        [Theory]
+        [InlineData(0)]
+        [InlineData(1)]
+        [InlineData(5)]
+        public void FindTrainerByGameId_ValidIds_TrainerCount(int trainerCount)
+        {
+            var game = GetTestGame();
+            DatabaseUtility.TryAddGame(game, out _);
+
+            Logger.WriteLine($"Adding {trainerCount} trainers to game id {game.GameId}");
+            var trainerIds = new List<string>();
+            for (int i = 0; i < trainerCount; i++)
+            {
+                var trainer = GetTestTrainer();
+                trainer.GameId = game.GameId;
+                DatabaseUtility.TryAddTrainer(trainer, out _);
+                trainerIds.Add(trainer.TrainerId);
+            }
+
+            Logger.WriteLine($"Retrieving list of trainerIds with game id {game.GameId}");
+            var retrievedTrainerIds = DatabaseUtility.FindTrainersByGameId(game.GameId).Select(trainer => trainer.TrainerId);
+            DatabaseUtility.DeleteGame(game.GameId);
+            foreach (var trainerId in retrievedTrainerIds)
+            {
+                DatabaseUtility.DeleteTrainer(trainerId);
+            }
+
+            Logger.WriteLine($"Verifying that the retrieved list matches the original set");
+            Assert.True(retrievedTrainerIds.All(trainerId => trainerIds.Contains(trainerId)));
+            Assert.Equal(trainerCount, retrievedTrainerIds.Count());
+        }
+
+        [Fact]
+        public void FindTrainerByGameId_NullId_EmptyList()
+        {
+            Logger.WriteLine($"Retrieving trainer list");
+            var trainerIds = DatabaseUtility.FindTrainersByGameId(null);
+            Logger.WriteLine($"Verifying that the retrieved list is empty");
+            Assert.Empty(trainerIds);
+        }
+
+        [Theory]
+        [InlineData("testusername")]
+        [InlineData("testusername1")]
+        public void FindTrainerByUsername_ValidUsername_TrainerModel(string username)
+        {
+            var trainer = GetTestTrainer();
+            trainer.TrainerName = username;            
+            Logger.WriteLine($"Adding trainer with username {username}");
+            DatabaseUtility.TryAddTrainer(trainer, out _);
+
+            Logger.WriteLine($"Retrieving trainer with username {username}");
+            var retrievedTrainer = DatabaseUtility.FindTrainerByUsername
+            (
+                username,
+                trainer.GameId
+            );
+
+            Logger.WriteLine($"Verifying that the trainer found is the correct trainer");
+            DatabaseUtility.DeleteTrainer(trainer.TrainerId);
+            Assert.Equal(username, retrievedTrainer.TrainerName);
+            Assert.Equal(trainer.GameId, retrievedTrainer.GameId);
+            Assert.Equal(trainer.TrainerId, retrievedTrainer.TrainerId);
+        }
+
+        [Theory]
+        [InlineData(null)]
+        [InlineData("")]
+        [InlineData("A name that doesn't exist")]
+        public void FindTrainerByUsername_InvalidUsername_Null(string username)
+        {
+            var trainer = GetTestTrainer();
+            Logger.WriteLine($"Adding trainer with username {trainer.TrainerName}");
+            DatabaseUtility.TryAddTrainer(trainer, out _);
+
+            Logger.WriteLine($"Retrieving trainer with username {username}");
+            var retrievedTrainer = DatabaseUtility.FindTrainerByUsername
+            (
+                username,
+                trainer.GameId
+            );
+
+            Logger.WriteLine($"Verifying that no trainer was found");
+            DatabaseUtility.DeleteTrainer(trainer.TrainerId);
+            Assert.Null(retrievedTrainer);
         }
     }
 }
