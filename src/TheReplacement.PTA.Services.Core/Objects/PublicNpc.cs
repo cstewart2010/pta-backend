fc9ed1203c9c42e64959a9669109791128f5a272
Delyx <daclaxton6@gmail.com>
2022-03-31 21:13:53 -0400

    Created new publicNpc class

---
+++ src/TheReplacement.PTA.Services.Core/Objects/PublicNpc.cs
@@ -0,0 +1,92 @@
+﻿using Newtonsoft.Json.Linq;
+using System.Collections.Generic;
+using System.Linq;
+using TheReplacement.PTA.Common.Models;
+using TheReplacement.PTA.Common.Utilities;
+
+namespace TheReplacement.PTA.Services.Core.Objects
+{
+    public class PublicNpc
+    {
+        internal static PublicNpc FromJson(JToken json)
+        {
+            return json.ToObject<PublicNpc>();
+        }
+
+        internal PublicNpc() { }
+
+        internal PublicNpc(NpcModel npc)
+        {
+            NpcId = npc.NPCId;
+            //GameId = npc.GameId;  
+            TrainerName = npc.TrainerName;
+            Feats = npc.Feats;
+            TrainerClasses = npc.TrainerClasses;
+            TrainerStats = npc.TrainerStats;
+            var NpcPokemon = DatabaseUtility.FindPokemonByTrainerId(npc.NPCId);
+            PokemonTeam = NpcPokemon
+                .Where(Pokemon => Pokemon.IsOnActiveTeam);
+            //Level = npc.Level;
+            //TrainerSkills = npc.TrainerSkills;
+           // Gender = npc.Gender;
+            //Height = npc.Height;
+           // Weight = npc.Weight;
+           // Description = npc.Description;
+           // Personality = npc.Personality;
+            //Background = npc.Background;
+            //Goals = npc.Goals;
+        }
+
+       /* internal NpcModel parseBackToModel()
+        {
+            var npc = DatabaseUtility.FindTrainerById(NpcId);
+            npc.TrainerName = TrainerName;
+            npc.Feats = Feats;
+            npc.TrainerClasses = TrainerClasses;
+            npc.TrainerStats = TrainerStats;   
+            npc.Level = Level;
+            npc.TrainerSkills = TrainerSkills;
+            npc.Gender = Gender
+            npc.Height = Height;
+            npc.Weight = Weight; 
+            npc.Description = Description;
+            npc.Personality = Personality;
+            npc.Background = Background;
+            npc.Goals = Goals;
+            return npc;
+        }*/
+
+        public string NpcId { get; set; }
+
+        public string GameId { get; set; }
+
+        public string TrainerName { get; set; }
+
+        public IEnumerable<string> Feats { get; set; }
+
+        public IEnumerable<string> TrainerClasses { get; set; }
+
+        public StatsModel TrainerStats { get; set; }
+
+        public IEnumerable<PokemonModel> PokemonTeam { get; set; }
+
+        public int Level { get; set; }
+        public IEnumerable<TrainerSkill> TrainerSkills { get; set; }
+
+        public string Gender { get; set; }
+
+        public int Height { get; set; }
+
+        public int Weight { get; set; }
+
+        public string Description { get; set; }
+
+        public string Personality { get; set; }
+
+        public string Background { get; set; }
+
+        public string Goals { get; set; }
+
+        public string Species { get; set; }
+    }
+}
