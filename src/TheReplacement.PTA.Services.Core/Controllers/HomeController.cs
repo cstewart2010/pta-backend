b64e66810ac19fb8f1f513e22b885ec18addb7a9
Chahlie <cstewart2010@gmail.com>
2022-10-21 01:56:15 -0500

    test 2

--- src/TheReplacement.PTA.Services.Core/Controllers/HomeController.cs
+++ src/TheReplacement.PTA.Services.Core/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using System;
 using System.Reflection;
 
 namespace TheReplacement.PTA.Services.Core.Controllers
@@ -35,6 +36,9 @@ namespace TheReplacement.PTA.Services.Core.Controllers
                 Movedex = $"{HostUrl}/{Version}/movedex",
                 Origindex = $"{HostUrl}/{Version}/origindex",
                 Classdex = $"{HostUrl}/{Version}/classdex",
+                DatabaseProcess = Environment.GetEnvironmentVariable("Database", EnvironmentVariableTarget.Process),
+                DatabaseUser = Environment.GetEnvironmentVariable("Database", EnvironmentVariableTarget.User),
+                DatabaseMachine = Environment.GetEnvironmentVariable("Database", EnvironmentVariableTarget.Machine),
             };
         }
     }
